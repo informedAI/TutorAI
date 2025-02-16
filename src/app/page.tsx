e681ae395e2513f50e300b49c5c77898d1875b1b
Jakub Madejczyk <jbmk01@gmail.com>
2025-02-16 20:17:54 +0100

    Fixed mobile About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 const Welcome = () => {
   return (
-    <main className="min-h-full min-w-full flex items-center justify-center flex-col gap-4 text-center">
+    <main className="min-h-screen min-w-full flex items-center justify-center flex-col gap-4 text-center text-xl md:text-3xl font-londrinaSolid p-8">
       {" "}
-      <p className="text-5xl drop-shadow-sm font-londrinaShadow">
+      <p className="text-3xl md:text-5xl drop-shadow-sm font-londrinaShadow">
         Studying is important, right?
       </p>{" "}
-      <p className="text-3xl font-londrinaSolid">
-        You know what else is important? Sleep.{" "}
-      </p>
-      <p className="text-3xl font-londrinaSolid text-white">
+      <p className="">You know what else is important? Sleep. </p>
+      <p className=" text-white">
         We made an app so you don&apos;t have to give up any of those two.
       </p>
-      <p className="text-3xl font-londrinaSolid">
+      <p className="">
         Save time on creating materials instead of actually studying them.
       </p>
-      <p className="text-3xl font-londrinaSolid text-white">
-        Made by students for students. ❤️
-      </p>
+      <p className=" text-white">Made by students for students. ❤️</p>
     </main>
   );
 };
